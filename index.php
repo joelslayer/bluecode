33d533f7a01474ce7d3e02fc0abd10d4c7b478b2
Joel Gomes dos Santos <j.k.slayer@hotmail.com>
2018-08-03 15:31:32 -0300

    Primeiro Commit - Inicio do Projeto, Estrutura HTML montada

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, user-scalable=0"/>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/normalize.css"/>
+	<link rel="stylesheet" type="text/css" href="assets/css/template.css"/>
+
+	<script type="text/javascript" src="assets/js/script.js"></script>
+
+	<title>BLUE-CODE</title>
+</head>
+<body>
+
+	<section class="topo-bar">
+		<div class="container">
+			<div class="items-corner">
+				<div class="logo">LOGO</div>
+				<nav class="menu">
+					<ul>
+						<li><a href="">Link 1</a></li>
+						<li><a href="">Link 1</a></li>
+						<li><a href="">Link 1</a></li>
+						<li><a href="">Link 1</a></li>
+						<li><a href="">Link 1</a></li>
+					</ul>
+				</nav>
+			</div>			
+		</div>
+	</section>
+
+	<section class="slideshow">
+		<div class="slideshow-area">
+			<div class="slide">
+				<div class="headline">
+					<div class="headline-items">
+						<div class="title1">1</div>
+						<div class="description1">1</div>
+						<input type="button" value="link"/>
+					</div>
+				</div>
+			</div>
+			<div class="slide">
+				<div class="headline">
+					<div class="headline-items">
+						<div class="title1">1</div>
+						<div class="description1">1</div>
+						<input type="button" value="link"/>
+					</div>
+				</div>
+			</div>
+			<div class="slide">
+				<div class="headline">
+					<div class="headline-items">
+						<div class="title1">1</div>
+						<div class="description1">1</div>
+						<input type="button" value="link"/>
+					</div>
+				</div>
+			</div>
+			<div class="slide">
+				<div class="headline">
+					<div class="headline-items">
+						<div class="title1">1</div>
+						<div class="description1">1</div>
+						<input type="button" value="link"/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="corpo">
+		<div class="container">
+			<div class="items-corner">
+				<div class="square1">
+					<div class="title2">html</div>
+					<div class="link-bar">confira</div>
+				</div>
+				<div class="square2">
+					<div class="title2">html</div>
+					<div class="link-bar">confira</div>
+				</div>
+				<div class="square3">
+					<div class="title2">html</div>
+					<div class="link-bar">confira</div>
+				</div>
+			</div>	
+		</div>
+	</section>
+
+	<footer class="rodape">
+		<div class="container">
+			<div class="items-corner">
+				<div class="rodape-area1">
+					<div class="title3">title 3</div>
+					<div class="description3">desc3</div>
+					<div class="description3">desc3</div>
+				</div>
+				<div class="rodape-area2">
+					<div class="title3">title 3</div>
+					<img src=""/>
+				</div>
+				<div class="rodape-area3">
+					<div class="title3">title 3</div>
+					<img src=""/>
+					<img src=""/>
+					<img src=""/>
+					<img src=""/>
+				</div>
+			</div>	
+			<div class="items-corner">
+				<div class="menu-bottom">
+					<ul>
+						<li><a href="">Link1</a></li>
+						<li><a href="">Link1</a></li>
+						<li><a href="">Link1</a></li>
+					</ul>
+				</div>
+
+				<div class="designed">
+					<div class="rights">Rights</div>
+					<div class="desenvolvedor">JtDevelop</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
