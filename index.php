73b3f9125780dca4e6bb2149b9ed7e30820e5869
Joel Gomes dos Santos <j.k.slayer@hotmail.com>
2018-08-06 16:22:54 -0300

    Atualização do corpo e do Rodape da página

--- index.php
+++ index.php
@@ -92,37 +92,42 @@
 		</div>
 	</section>
 
-	<footer class="rodape">
-		<div class="container">
-			<div class="items-corner">
-				<div class="rodape-area1">
-					<div class="title3">title 3</div>
-					<div class="description3">desc3</div>
-					<div class="description3">desc3</div>
+	<footer class="rodape justify-center">
+		<div class="container column">
+			<div class="items-corner space-between">
+				<div class="rodape-area1 column">
+					<div class="title3">BLUE CODE</div>
+					<div class="rodape-info">
+						<div class="description3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam.</div>
+						<div class="description3">TEL: (xx) X -XXXX-XXXX</div>
+						<div class="description3">EMAIL: NOME@DOMINIO.COM.BR</div>
+					</div>
 				</div>
-				<div class="rodape-area2">
-					<div class="title3">title 3</div>
-					<img src=""/>
+				<div class="rodape-area2 column">
+					<div class="title3">LOCALIZAÇÃO</div>
+					<img src="assets/img/mapa.png" height="90" />
 				</div>
-				<div class="rodape-area3">
-					<div class="title3">title 3</div>
-					<img src=""/>
-					<img src=""/>
-					<img src=""/>
-					<img src=""/>
+				<div class="rodape-area3 column">
+					<div class="title3">REDES SOCIAIS</div>
+					<div class="rodape-info">
+						<img src="assets/img/Facebook.png" height="36" />
+						<img src="assets/img/Instagram.png" height="36" />
+						<img src="assets/img/Whatsapp.png" height="36" />
+						<img src="assets/img/Mail.png" height="36" />
+					</div>
 				</div>
 			</div>	
-			<div class="items-corner">
+			<div class="items-corner space-between flex-wrap">
 				<div class="menu-bottom">
 					<ul>
-						<li><a href="">Link1</a></li>
-						<li><a href="">Link1</a></li>
-						<li><a href="">Link1</a></li>
+						<li><a href="">HOME</a></li>
+						<li><a href="">SERVICES</a></li>
+						<li><a href="">CONTACT</a></li>
 					</ul>
 				</div>
 
 				<div class="designed">
-					<div class="rights">Rights</div>
+					<div class="rights">All Rights reserved!</div>
 					<div class="desenvolvedor">JtDevelop</div>
 				</div>
 			</div>
